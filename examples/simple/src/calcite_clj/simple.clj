ca54f84e40a1a357244ce394e61636b731d00593
Eugen Stan <eugen.stan@netdava.com>
2022-01-07 23:57:29 +0200

    Implemented example with in memory clojure strcutures

--- examples/simple/src/calcite_clj/simple.clj
+++ examples/simple/src/calcite_clj/simple.clj
@@ -5,7 +5,9 @@
   (:import (java.util List
                       Map
                       Map$Entry)
-           (org.apache.calcite DataContext)
+           (java.util.concurrent.atomic AtomicBoolean)
+           (org.apache.calcite DataContext
+                               DataContext$Variable)
            (org.apache.calcite.adapter.java JavaTypeFactory)
            (org.apache.calcite.jdbc JavaTypeFactoryImpl)
            (org.apache.calcite.linq4j AbstractEnumerable)
@@ -22,15 +24,17 @@
 (set! *warn-on-reflection* true)
 
 ;; DEPTNO:int	NAME:string
-(def depts {:rowdef [{:name "DEPTNO" :type SqlTypeName/INTEGER}
+(def depts {:name "DEPTS"
+            :rowdef [{:name "DEPTNO" :type SqlTypeName/INTEGER}
                      {:name "NAME" :type SqlTypeName/VARCHAR}]
-            :data [[10 "Sales"]
-                   [20	"Marketing"]
-                   [30 "Accounts"]]})
+            :data [[(int 10) "Sales"]
+                   [(int 20)	"Marketing"]
+                   [(int 30) "Accounts"]]})
 
 ;; EMPNO:int	NAME:string	DEPTNO:int	GENDER:string	CITY:string	EMPID:int
 ;; AGE:int	SLACKER:boolean	MANAGER:boolean	JOINEDAT:date
-(def emps {:rowdef [{:name "EMPNO" :type SqlTypeName/INTEGER}
+(def emps {:name "EMPS"
+           :rowdef [{:name "EMPNO" :type SqlTypeName/INTEGER}
                     {:name "NAME" :type SqlTypeName/VARCHAR}
                     {:name "DEPTNO" :type SqlTypeName/INTEGER}
                     {:name "GENDER" :type SqlTypeName/VARCHAR}
@@ -39,12 +43,12 @@
                     {:name "AGE" :type SqlTypeName/INTEGER}
                     {:name "SLACKER" :type SqlTypeName/BOOLEAN}
                     {:name "MANAGER" :type SqlTypeName/BOOLEAN}
-                    {:name "JOINDATE" :type SqlTypeName/DATE}]
-           :data [[100 "Fred" 10 nil nil 30 25 true false "1996-08-03"]
-                  [110	"Eric"	20	"M"	"San Francisco"	3	80		nil false	"2001-01-01"]
-                  [110	"John"	40	"M"	"Vancouver"	2	false	true	"2002-05-03"]
-                  [120	"Wilma"	20	"F"	 nil	1	5	nil true "2005-09-07"]
-                  [130	"Alice"	40	"F"	"Vancouver"	2	false	true	"2007-01-01"]]})
+                    {:name "JOINDATE" :type SqlTypeName/VARCHAR}]
+           :data [[(int 100) "Fred" (int 10) nil nil             (int 30) (int 25) true false "1996-08-03"]
+                  [(int 110) "Eric"	(int 20) "M" "San Francisco" (int 3)	(int 80)	nil false	"2001-01-01"]
+                  [(int 110) "John"	(int 40) "M" "Vancouver"	   (int 2)	nil false	true	"2002-05-03"]
+                  [(int 120) "Wilma" (int 20)	"F"	nil	           (int 1)	(int 5)	nil true "2005-09-07"]
+                  [(int 130) "Alice" (int 40)	"F"	"Vancouver"	   (int 2)	nil false	true	"2007-01-01"]]})
 
 (defn to-nullable-rel-data-type
   "Helper to create rel-data-type"
@@ -59,36 +63,46 @@
 (defn row-type
   "Build a calcite row type (reltype)from a clojure structure"
   ^RelDataType [^JavaTypeFactory type-factory rowdef]
+  (println "row-type" rowdef)
   (.createStructType type-factory ^List (map (partial row->rel-type type-factory) rowdef)))
 
 (defn scannable-table
   "Table based on a clojure data structure.
    It implements the {@link ScannableTable} interface, so Calcite gets
    data by calling the {@link #scan(DataContext)} method."
-  ^Table [{:keys [data rowdef] :as table-def}]
-  (let []
+  ^Table [{:keys [data rowdef name] :as table-def}]
+  (let [row-type-memo (memoize row-type)]
     (proxy+
      []
      AbstractTable
      (getRowType
       [this ^RelDataTypeFactory type-factory]
-      (row-type type-factory rowdef))
+      (row-type-memo type-factory rowdef))
 
      ScannableTable
      (scan
       [this ^DataContext root]
       (let [type-factory (.getTypeFactory root)
-            ;; field-types (get-field-types type-factory source field-types-x is-stream)
-            ;; fields (ImmutableIntList/identity (.size field-types))
-            ;; cancel-flag (.get DataContext$Variable/CANCEL_FLAG root)
-            ]
+            ^AtomicBoolean cancel-flag (.get DataContext$Variable/CANCEL_FLAG root)]
         (proxy+
          []
          AbstractEnumerable
          (enumerator
           [this]
-          (throw (UnsupportedOperationException. "Not implemented"))
-          )))))))
+          (let [i (atom -1)
+                len (- (count data) 1)]
+            (reify org.apache.calcite.linq4j.Enumerator
+              (current [this] (into-array Object (get data @i)))
+              (moveNext
+                [this]
+                (let [res (< @i len)]
+                  (if (.get cancel-flag)
+                    false
+                    (do
+                      (when res (swap! i inc))
+                      res))))
+              (reset [this] (reset! i -1))
+              (close [this] nil))))))))))
 
 (defn create-tables
   ^Map []
@@ -113,8 +127,8 @@
     (row-type jtf (:rowdef emps)))
 
 
-  #_(require '[clojure.tools.trace :as trace])
-  #_(trace/trace-ns 'ro.ieugen.calcite-csv)
+  (require '[clojure.tools.trace :as trace])
+  (trace/trace-ns 'calcite-clj.simple)
 
   (let [db {:jdbcUrl "jdbc:calcite:model=resources/model.json"
             :user "admin"
@@ -130,5 +144,4 @@
     (with-open [connection (jdbc/get-connection ds)]
       (let [metadata (.getMetaData connection)
             rs (.getTables metadata nil nil nil (into-array ["TABLE" "VIEW"]))]
-        (rs/datafiable-result-set rs ds))))
-  )
+        (rs/datafiable-result-set rs ds)))))
